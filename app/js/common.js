29f5be7d2fecac58a7e29e2185561bfa755e122d
user <user@Gapur.local>
2018-11-06 23:16:15 +0300

    lasdjf

--- app/js/common.js
+++ app/js/common.js
@@ -2,7 +2,7 @@ $(function() {
 
 	$('.category__title').click(function(){
 		$(this).toggleClass('open', 1000);
-		$(this).next('.category__list').toggleClass('open', 1000);
+		$(this).next('.category__list').toggleClass('open', 300);
 	});
 	
 	$("#slider_price").slider({
@@ -18,6 +18,30 @@ $(function() {
 
 	$('input[name="minPrice"]').val( '$' + $("#slider_price").slider('values',0) );
 	$('input[name="maxPrice"]').val( '$' + $("#slider_price").slider('values',1) );
+
+	$('#cart, .title_cart').click(function(){
+		$('#cart_box').toggleClass('open');
+	});
+
+	$('#sign').click(function(){
+		$('#black_fill').toggleClass('open');
+		$('#modal').toggleClass('open');
+	});
+
+	$('#close_modal, #black_fill').click(function(){
+		$('#black_fill').toggleClass('open');
+		$('#modal').toggleClass('open');
+	});
+
+	$('#search').click(function(){
+		$('#search_panel').toggleClass('open');
+	});
+
+
+	// $('#load_more').click(function(event){
+	// 	event.preventDefault();
+	// 	$('.list_product').append('<a href="#" class="product box main_flex__nowrap flex__jcontent_center flex__align-items_center"><span class="tag">new</span><span class="product__img"><img src="img/products/01.png"></span><h2>Our Legacy Splash Jacquard Knit</h2><p class="color">Black Grey Plants</p><p class="price">$290</p></a><a href="#" class="product box main_flex__nowrap flex__jcontent_center flex__align-items_center"><span class="tag">new</span><span class="product__img"><img src="img/products/01.png"></span><h2>Our Legacy Splash Jacquard Knit</h2><p class="color">Black Grey Plants</p><p class="price">$290</p></a><a href="#" class="product box main_flex__nowrap flex__jcontent_center flex__align-items_center"><span class="tag">new</span><span class="product__img"><img src="img/products/01.png"></span><h2>Our Legacy Splash Jacquard Knit</h2><p class="color">Black Grey Plants</p><p class="price">$290</p></a>');
+	// });
 
 
 });
